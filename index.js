8734480dae67d30baff040da86b0788a6ae63a18
Sangamita <sangamitashakya@hotmail.com>
2024-10-14 15:17:54 +0200

    start javasript

---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+
+var a = 10;
+const pi = 3.145;
+let area = 20;
+console.log(a);
+console.log(pi);
+console.log(area);
+
+// strings
+let firstName = "Sabin";
+let lastName = "Bajracharya";
+console.log(firstName + " " + lastName)
+console.log(`My name is ${firstName} ${lastName}`)
+
+//strings method or functions
+//length functions
+console.log(lastName.length)
+// trim functions
+console.log(`      Hello      ${firstName}   `.trim().length);
+
+console.log(firstName.toLowerCase());
+console.log(lastName.toUpperCase());
+
+//split functions
+console.log(`Hello my name is sabin`.split(" "));
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// numbers in javascripts
+let example = 7.567;
+console.log(typeof example);
+let intNumber = parseInt(example);
+console.log(intNumber)
+console.log(parseFloat(example))
+
+let number = '4.567';
+console.log(parseInt(number))
+console.log(parseFloat(number).toFixed(1))
+
+
+
+let example1 = parseInt("22 and 33 Namaste");
+let example2 = parseFloat('44 sabin 33');
+let example3 = 44.567.toFixed(0)
+let example4 = 100.0.toFixed(5)
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+
+console.log(typeof example1);
+console.log(typeof example2);
+console.log(typeof example3);
+console.log(typeof example4);
+
+*/
+
+//boolean values
+// let example1 = 1233;
+// let example2 = false;
+
+// console.log(Boolean(example1));
+
+// let example1 = false;
+// let example2 = true;
+// let example3 = null;
+// let example4 = undefined;
+// let example5 = '';
+// let example6 = NaN;
+// let example7 = -5;
+// let example8 = 0;
+
+// console.log(Boolean(example1));
+// console.log(Boolean(example2));
+// console.log(Boolean(example3));
+// console.log(Boolean(example4));
+// console.log(Boolean(example5));
+// console.log(Boolean(example6));
+// console.log(Boolean(example7));
+// console.log(Boolean(example8));
+
+
+let example
